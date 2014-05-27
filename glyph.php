7b5c7ab26c1513f46df38ff9bcebb1684e3c35bd
Pablo Impallari <impallari@gmail.com>
2014-05-27 14:17:54 -0300

    Small updates
    Indentation
    Properly closing HEAD and HTML tags
    Removed unused comments

--- glyph.php
+++ glyph.php
@@ -1,10 +1,12 @@
-<?php include('_functions.php');
+<?php
+header("Content-type: text/html; charset=utf-8"); // UTF 8
+include('_functions.php');
 include('fonts_list.php');
-$nombre=$_GET['nombre'];
-$glifo=$signo[$nombre];
-$char=uni($glifo['char']);
+
+$nombre = $_GET['nombre'];
+$glifo = $signo[$nombre];
+$char = uni($glifo['char']);
 	
-#$fonts=array('FreeSans','FreeSerif','EkMukta','Fedra','KunKun','Lohit','NotoSans','Saral','Devanagari Sangam MN','Adobe Devanagari','Arial');
 foreach ($fuentes as $fuente){
 	$nombre_fuente=substr($fuente, 0, -4);
 	$thumbsBig.='<div class="thumbBig" style="font-family:'.$nombre_fuente.'">'.$char.'<label>'.$nombre_fuente.'</label></div>'."\n";
@@ -27,57 +29,66 @@ foreach($words as $word){
 		array_push($wordlist, $word);
 	}
 }
+
 #arma palabras aleatorias hasta 50
 for($i=0;$i<100;$i++){
 	$index=rand(0,(count($wordlist)-1));
 	$textoPrueba.=$wordlist[$index].' ';
 }
-#foreach($wordlist as $word){
-#	$textoPrueba.=$word.' ';
-#}
+
 ?>
-
 <!DOCTYPE html>
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta name="author" content="Deva">
 <title>Devanaguide- <? echo $glifo['nombre']?></title>
-<link type="text/css" href="css/estilos.css" rel="stylesheet" charset="utf-8">
 <style>
 <? echo $css?>
 </style>
+<link type="text/css" href="css/estilos.css" rel="stylesheet" charset="utf-8">
+</head>
+
 <body>
+
 	<div id="left">
 		<div class="container">
-		<a href="index.php"><< Menu</a><br /><br />
-		<table id="data" cellpading=0 cellspacing=0>
-		<tr><th>Categoria</th><td><? echo $glifo['categoria']?></td></tr>
-		<tr><th>Subcategoría</th><td><? echo $glifo['subcategoria']?></td></tr>
-		<tr><th>Nombre</th><td><? echo $glifo['nombre']?></td></tr>
-		<tr><th>Char</th><td><? echo uni($glifo['char'])?></td></tr>
-		<tr><th>Unicode</th><td><? echo $glifo['char']?></td></tr>
-		</table>
-		<h3>Descripcion</h3>
-		<? echo $glifo['descripcion']?>
-
+			
+			<a href="index.php"><< Menu</a><br /><br />
+			<table id="data" cellpading=0 cellspacing=0>
+				<tr><th>Categoria</th><td><? echo $glifo['categoria']?></td></tr>
+				<tr><th>Subcategoría</th><td><? echo $glifo['subcategoria']?></td></tr>
+				<tr><th>Nombre</th><td><? echo $glifo['nombre']?></td></tr>
+				<tr><th>Char</th><td><? echo uni($glifo['char'])?></td></tr>
+				<tr><th>Unicode</th><td><? echo $glifo['char']?></td></tr>
+			</table>
+			<h3>Descripcion</h3>
+			<? echo $glifo['descripcion']?>
+			
 		</div>
 	</div>
+	
 	<div id="main">
-	<div class="container">
-
-<h2>Ejemplos de <? echo $glifo['nombre'] ?></h2>
-<div class="thumbs">
-
-	<? echo $thumbsBig ?>
-</div>
-<div class="thumbs">
-	<? echo $thumbsWord ?>
-</div>
-<h2>Palabras</h2>
-<p><? echo 'Mostrando 100 de '.count($wordlist).' palabras encontradas. ('.count($words).' total)'?></p>
-<div class="contexto">
-<? echo $textoPrueba?>
-</div>
-</div>
-</div>
+		<div class="container">
+			
+			<h2><? echo $glifo['nombre'] ?></h2>
+			
+			<div class="thumbs">
+				<? echo $thumbsBig ?>
+			</div>
+			
+			<div class="thumbs">
+				<? echo $thumbsWord ?>
+			</div>
+			
+			<h2>Words containing <? echo $glifo['nombre'] ?></h2>
+			<p><? echo 'Mostrando 100 de '.count($wordlist).' palabras encontradas. ('.count($words).' total)'?></p>
+			<div class="contexto">
+				<? echo $textoPrueba?>
+			</div>
+			
+		</div>
+	</div>
+	
 </body>
+</html>
--- index.php
+++ index.php
@@ -1,25 +1,34 @@
-<?php include('_functions.php') ?>
+<?php
+header("Content-type: text/html; charset=utf-8"); // UTF 8
+include('_functions.php');
+?>
 <!DOCTYPE html>
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="author" content="Deva">
+	<title>Devanaguide</title>
+	<link type="text/css" href="css/estilos.css" rel="stylesheet" charset="utf-8">
+</head>
 
-<meta name="author" content="Deva">
-<title>Devanaguide</title>
-<link type="text/css" href="css/estilos.css" rel="stylesheet" charset="utf-8">
 <body class="signos">
+
 	<div class="header">
 		<h1>Devanaguide</h1>
 		<p>Huerta Tipográfica</p>
 	</div>
+	
 	<div class="menu">
-	<?
-	foreach ($signo as $glifo){
-		if($glifo['char']!='None'){
-		echo '<a href="glyph.php?nombre='.$glifo['nombre'].'" title="'.$glifo['nombre'].'"><span class="char">'.uni($glifo['char']).'</span><span class="nombre">'.$glifo['nombre'].'</span></a>'."\n";
-		#echo $glifo['nombre'];
-	}
-	};
-	?>
+		<?
+		foreach ($signo as $glifo){
+			if($glifo['char']!='None'){
+			echo '<a href="glyph.php?nombre='.$glifo['nombre'].'" title="'.$glifo['nombre'].'"><span class="char">'.uni($glifo['char']).'</span><span class="nombre">'.$glifo['nombre'].'</span></a>'."\n";
+			#echo $glifo['nombre'];
+			}
+		};
+		?>
 	</div>
+	
 	<div class="header">
 		<p><a href="http://devanaguide.huertatipografica.com">Devanaguide</a> Vesion 0.1 | Desarrollado por Andrés Torresi | <a href="http://www.huertatipografica.com/">Huerta Tipográfica</a> (andres@huertatipografica.com) | <a href="https://github.com/andrestelex/devanaguide">Contribute on Github</a></p>
 		<p>Podés descargar y modificar este software | Feel free to modify this software</p>
@@ -29,4 +38,6 @@
 		<p>Dictionary words from <a href="http://sanskritdocuments.org/hindi/dict/eng-hin_unic.html">http://sanskritdocuments.org/hindi/dict/eng-hin_unic.html</a></p>
 		<p>Mostrando solo glifos con unicode | Showing only unicode glyphs</p>
 	</div>
+	
 </body>
+</html>
