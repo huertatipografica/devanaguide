292f8194710d520e995e785c6f746725831d05ef
Juan <juandelperal@gmail.com>
2014-06-06 11:34:14 -0300

    added to fonts list

--- fonts_list.php
+++ fonts_list.php
@@ -1,33 +1,36 @@
 <?
-$dir='fonts';
+$dir=['fonts', 'fonts/otras'];
 $fuentes=array();
 
 #lista de fuentes
-if ($handle = opendir($dir)) {
+foreach ($dir as $dir) {
+    if ($handle = opendir($dir)) {
 
-    /* This is the correct way to loop over the directory. */
-    while (false !== ($entry = readdir($handle))) {
-		#solo si son otf o ttf
-		if(strpos($entry,".otf") or strpos($entry,".ttf")){
-			array_push($fuentes, $entry);
-		}
-		
+        /* This is the correct way to loop over the directory. */
+        while (false !== ($entry = readdir($handle))) {
+        #solo si son otf o ttf
+            if(strpos($entry,".otf") or strpos($entry,".ttf")){
+                array_push($fuentes, $entry);
+            }
+
+        }
+        closedir($handle);
     }
-    closedir($handle);
 }
 
-#echo '<pre>';
-#print_r($fuentes);
-#echo '</pre>';
+
+echo '<pre>';
+print_r($fuentes);
+echo '</pre>';
 
 natsort($fuentes);
 foreach($fuentes as $fuente){
 	$css.="@font-face {
-    font-family: '".substr($fuente, 0, -4)."';
-    src: url('".$dir."/".$fuente."');
-    font-weight: normal;
-    font-style: normal;
-}\n";
+        font-family: '".substr($fuente, 0, -4)."';
+        src: url('".$dir."/".$fuente."');
+        font-weight: normal;
+        font-style: normal;
+    }\n";
 };
 
 ?>
